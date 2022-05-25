5f38e79af0f4d9abb4e7782b24d3dc09ca3ae723
hidenlearner <cryptohashlock@gmail.com>
2022-05-25 21:45:07 +0600

    style ready

---
+++ index.js
@@ -0,0 +1,4 @@
+const amount = document.getElementById("amount");
+const foodamount = document.getElementById("foodamount");
+const clothamount = document.getElementById("clothamount");
+const rentamount = document.getElementById("rentamount");
